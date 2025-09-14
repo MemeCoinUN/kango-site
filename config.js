1f86bd11b838111bc3cb8278b6e3abdd4bb35bc4
MemeCoinUN <memecoinuniversity@yahoo.com>
2025-09-13 21:57:37 -0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.KANGO_LINKS = {
-  moonshot: "https://moonshot.com/2xdbLNxtFCiBUZNK1QU218Xrm9ct3JGzG2MNNpJsmoon",
-  dexscreener: "https://dexscreener.com/solana/2xdbLNxtFCiBUZNK1QU218Xrm9ct3JGzG2MNNpJsmoon",
-  contract: "2xdbLNxtFCiBUZNK1QU218Xrm9ct3JGzG2MNNpJsmoon"
-};
